54a62063bf0723dd40df9000524737d811e026a0
Colin Leroy-Mira <colin@colino.net>
2019-10-01 10:55:07 +0200

    Disable pinch and other extra features we don't want in kiosk mode

--- app/config/defaults/start_chromium.sh
+++ app/config/defaults/start_chromium.sh
@@ -5,4 +5,4 @@ CHROMIUM_TEMP=/tmp/chromium
 sudo http mkdir -p $CHROMIUM_TEMP
 export HOME=$CHROMIUM_TEMP
 
-chromium --app=http://localhost --start-fullscreen --force-device-scale-factor=1.8
+chromium --app=http://localhost --start-fullscreen --force-device-scale-factor=1.8 --kiosk --disable-pinch --disable-translate --disable-crash-reporter --disable-infobars --check-for-update-interval=8640000
