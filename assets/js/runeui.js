61d10870a2073970bd7d323fbb45e7a7c28ca44c
Colin Leroy <colin@colino.net>
2020-04-05 17:15:27 +0200

    Add alphabetic scroller

--- assets/js/runeui.js
+++ assets/js/runeui.js
@@ -1633,6 +1633,8 @@ function populateDB(options){
             // console.log('highlighted entry = ', GUI.currentDBpos[GUI.currentDBpos[10]]);
         }
     }
+
+    $("#database-entries").sliderNav();
     var breadcrumb = $('span', '#db-currentpath');
     if (GUI.browsemode === 'album') {
         if (path === 'Albums') {
