2ac9176ca717faeb8731117ebbf165d46d9b2b37
janui <janui@onmat.com>
2021-08-28 16:07:54 +0200

    Cosmetic changes

--- app/templates/mpd.php
+++ app/templates/mpd.php
@@ -188,7 +188,7 @@
                 <div class="col-sm-10">
                     <input class="form-control osk-trigger input-lg" type="number" id="audio-buffer-size" name="conf[audio_buffer_size]" value="<?=$this->conf['audio_buffer_size'] ?>" data-trigger="change" min="512" />
                     <span class="help-block">This specifies the size of the audio buffer in kilobytes. The default is 4096, large enough for nearly 24 seconds of CD-quality audio.<br>
-                    <i>Increase the size if you have a very slow NAS or experience stuttering playback problems (e.g. with high-definition audio files)</i></span>
+                    <i>Increase the size if you have very slow storage or experience stuttering playback problems (e.g. with high-definition audio files)</i></span>
                 </div>
             </div>
             <?php if(!$this->mpdv21): ?>
