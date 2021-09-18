0103be55fada445cba9afb7e5c93fc7c8615898d
janui <janui@onmat.com>
2021-09-18 12:57:52 +0200

    Restart local browser (if enabled) after changing volume setting

--- app/mpd_ctl.php
+++ app/mpd_ctl.php
@@ -43,6 +43,10 @@
     // update MPD configuration
     if (isset($_POST['conf'])) {
         $jobID[] = wrk_control($redis, 'newjob', $data = array('wrkcmd' => 'mpdcfg', 'action' => 'update', 'args' => $_POST['conf']));
+        if ($redis->hGet('local_browser', 'enable') && ($_POST['conf']['mixer_type'] != $redis->hGet('globalrandom', 'mixer_type'))) {
+            // local browser is enabled and the mixer typ has changed, restart the local browser
+            $jobID[] = wrk_control($redis, 'newjob', $data = array('wrkcmd' => 'xorgserver', 'action' => 'restart'));
+        }
     }
     // manual MPD configuration
     if (isset($_POST['mpdconf'])) {
