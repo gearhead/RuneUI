4f5ce2b783b4966a706f37f8b3b968367fbb9099
janui <janui@onmat.com>
2021-11-19 16:17:33 +0100

    Modify udev driven php scripts not to run during the start-up sequence

--- app/libs/openredis.php
+++ app/libs/openredis.php
@@ -33,13 +33,20 @@
  *
  */
 // Connect to Redis backend
+// this file is included into all scripts which use redis
 // in the backup and other functions redis is stopped and restarted, it is possible that
 // redis is still starting when this code executes, so trap any errors
 $redisError = true;
 // a massive repeat value for looping, it should work on the first try
 //  but, if no redis is connected is made the rest will fail, so keep trying
 //  should only be a problem during start up
-$redisErrorCount = 30;
+// this can be overridden by setting the variable $redisOpenOneTime to true in the parent
+//  script, it will then run once and exit on failure
+if (isset($redisOpenOneTime) && $redisOpenOneTime) {
+    $redisErrorCount = 1;
+} else {
+    $redisErrorCount = 30;
+}
 $redisSleepTime = 2;
 while ($redisError) {
     try {
@@ -49,30 +56,35 @@ while ($redisError) {
             $redisError = false;
         } else {
             $redisError = true;
-            sleep($redisSleepTime);
             $redisErrorCount--;
         }
     }
     catch (Throwable $t) {
         // Executed only in PHP 7 and higher, will not match in PHP 5 and lower
         $redisError = true;
-        sleep($redisSleepTime);
         $redisErrorCount--;
     }
     catch (Exception $e) {
         // Executed only in PHP 5 and lower, will not be reached in PHP 7 and higher
         $redisError = true;
-        sleep($redisSleepTime);
         $redisErrorCount--;
     }
     if ($redisErrorCount <= 0) {
+        if (isset($redisOpenOneTime) && $redisOpenOneTime) {
+            // exit the script if $redisOpenOneTime is set and true
+            // exit() will be interpreted as a successful completion in bash
+            exit();
+        }
         // exit the loop
         $redisError = false;
         // try again but don't trap the error
         $redis = new Redis();
         $redis->pconnect('/run/redis/socket');
+    } else if ($redisError) {
+        // loop again, so sleep first
+        sleep($redisSleepTime);
     }
 }
-unset($redisError, $redisErrorCount, $redisSleepTime);
+unset($redisError, $redisErrorCount, $redisSleepTime, $redisOpenOneTime);
 // end open redis
 //
