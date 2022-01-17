582251b9a4e88f759d0abbfb5c23e5e3046cdd0a
janui <janui@onmat.com>
2022-01-17 11:48:13 +0100

    Bug fixes

--- app/playback_ctl.php
+++ app/playback_ctl.php
@@ -36,7 +36,7 @@
 $nowSeconds = microtime(true);
 // delay 2 second, so add 2 to the value
 $startAfterSeconds = $nowSconds + 2;
-wrk_control($redis, 'newjob', $data = array('wrkcmd' => 'ui_render', 'action' => 'seconds', 'args' => $startAfterMicroseconds));
+wrk_control($redis, 'newjob', $data = array('wrkcmd' => 'ui_render', 'action' => 'seconds', 'args' => $startAfterSeconds));
 $template->activePlayer = $redis->get('activePlayer');
 //
 // setup the display variables
