aa327749f90b6df8eb23cd28831829f33247874a
gearhead <ys3al35l@gmail.com>
2022-01-29 16:33:58 -0600

    Finally got this to work with the help of borine from bluealsa this works. If a BT audio device is added, udev will trigger the script which will stop mpd. If a source (phone) is added, it will allow playback via the current default alsa output. If a sink (headphones) is added all the script currently does is stop mpd. no default output is added. This *should* be done for mpd, shairport-sync and spotify.

--- command/bt-connect.sh
+++ command/bt-connect.sh
@@ -1,22 +1,26 @@
-#!/usr/bin/bash
-# bt-sink.sh
-# Runeaudio
-# shell script to deal with bluetooth sources and sinks
-# this is called by the bluetooth rule in 99-runeaudio.rules
-case $1 in
-"start")
-   #mpc stop
-   type=$(/usr/bin/bluealsa-aplay -L | /usr/bin/grep trusted | /usr/bin/cut -d ',' -f 3 | /usr/bin/tr -d -d " \t\n\r")
-   case $type in
-   "playback") # attached speaker
-   /usr/bin/mpc stop
-   ;;
-   "capture") # attached a source
-   /usr/bin/mpc stop
-   ;;
-   esac
-;;
-"stop")
-   /usr/bin/mpc play
-;;
+#!/usr/bin/sh
+# bt-connect.sh
+# shell script to switch off mpd so that a source connected
+# will play through alsa system to the default output.
+# also if a speaker/headset is added, it will set it as the default
+# output in mpd.conf
+case "$1" in
+add)
+# wait for PCM(s) to be created, with absolute timeout of 5 seconds.
+timeout 5 stdbuf -oL bluealsa-cli -q monitor | grep -q -m 1 ^PCMAdded || exit 0
+	case "$(/usr/bin/bluealsa-cli -q list-pcms | grep -o -E '(sink|source)$')" in
+	*sink*) # attached speaker(s)
+		mpc stop
+		# set default output in mpd.conf to BT
+		;;
+	*source*) # attached phone
+		mpc stop
+		# this is enough to allow a device to play through alsa
+		;;
+	esac
+	;;
+remove)
+	# remove the BT default if it was added to mpd.conf
+	mpc play
+	;;
 esac
