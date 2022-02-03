b3b99447d04b4d050b20ef762e62aab63d6c324b
janui <janui@onmat.com>
2022-02-03 16:41:18 +0100

    Set permissions an clean up: command/bt-connect.sh

--- command/bt-connect.sh
+++ command/bt-connect.sh
@@ -8,19 +8,19 @@ case "$1" in
 add)
 # wait for PCM(s) to be created, with absolute timeout of 5 seconds.
 timeout 5 stdbuf -oL bluealsa-cli -q monitor | grep -q -m 1 ^PCMAdded || exit 0
-	case "$(/usr/bin/bluealsa-cli -q list-pcms | grep -o -E '(sink|source)$')" in
-	*sink*) # attached speaker(s)
-		mpc stop
-		# set default output in mpd.conf to BT
-		;;
-	*source*) # attached phone
-		mpc stop
-		# this is enough to allow a device to play through alsa
-		;;
-	esac
-	;;
+    case "$(/usr/bin/bluealsa-cli -q list-pcms | grep -o -E '(sink|source)$')" in
+    *sink*) # attached speaker(s)
+        mpc stop
+        # set default output in mpd.conf to BT
+        ;;
+    *source*) # attached phone
+        mpc stop
+        # this is enough to allow a device to play through alsa
+        ;;
+    esac
+    ;;
 remove)
-	# remove the BT default if it was added to mpd.conf
-	mpc play
-	;;
+    # remove the BT default if it was added to mpd.conf
+    mpc play
+    ;;
 esac
