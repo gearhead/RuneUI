5626139839cfea8dcbb5159bda75315348c11d38
janui <janui@onmat.com>
2022-07-27 14:03:45 +0200

    Debug UI textual changes

--- app/templates/debug.php
+++ app/templates/debug.php
@@ -1,8 +1,8 @@
 <div class="container debug">
     <h1>DEBUG DATA</h1>
     <div class="boxed">
-        <p>Below is displayed the raw output of RuneUI's debug section. It contains some important informations that could help to diagnose problems.<br>
-        Please copy and paste it in your posts when asking for help <a href="http://www.runeaudio.com/forum/" title="RuneAudio Forum" target="_blank">in the forum</a>.</p>
+        <p>The raw output of RuneUI's debug section is displayed below. It contains important information that could help to diagnose problems.<br>
+        Please copy and paste the relevant parts it into your posts if you see something which may help. See the <a href="www.runeaudio.com/forum/" title="RuneAudio Forum" target="forum">forum</a>.</p>
         <form id="debug-buttons" class="button-list" method="post">
             <button class="btn btn-primary btn-lg" id="refresh" name="refresh" value="1" type="submit"> Refresh</button> &nbsp;
             <button class="btn btn-primary btn-lg"id="copyText" name="copyText" type="button"><i class="fa fa-copy sx"></i> Copy data to clipboard</button>
