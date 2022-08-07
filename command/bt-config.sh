6c41adca59a71431d7344bf5b296a4b37960d432
janui <janui@onmat.com>
2022-08-07 17:29:54 +0200

    Cosmetic format changes

--- command/bt-config.sh
+++ command/bt-config.sh
@@ -5,29 +5,29 @@
 file="/etc/mpd.conf"
 mpc stop
 systemctl stop mpd
-        sed -i '/audio_output/,/}/ s/enabled/#&/ ' $file
-        mapfile -t bts < <( aplay -L )
-        lines=$(aplay -L | wc -l )
-        let lines='lines-1'
-        if [ $lines -ne 0 ]; then
-            for i in $( eval echo {0..$lines});do
-                if echo "${bts[$i]}" | grep -q a2dp ; then
-                    name=$(echo ${bts[$i+1]} | cut -d "," -f 1)
-                    device=$(echo ${bts[$i]})
-                    echo "audio_output {" >> $file
-                    echo -ne "        name            \"" >> $file
-                    echo -ne $name >> $file
-                    echo "\"" >> $file
-                    echo "        type            \"alsa\"" >> $file
-                    echo -ne "        device          \"" >> $file
-                    echo -ne $device >> $file
-                    echo "\"" >> $file
-                    echo "        mixer_type      \"software\"" >> $file
-                    echo "        auto_resample   \"no\"" >> $file
-                    echo "        auto_format     \"no\"" >> $file
-                    echo "        enabled         \"yes\"" >> $file
-                    echo "}" >> $file
-                fi
-            done
+sed -i '/audio_output/,/}/ s/enabled/#&/ ' $file
+mapfile -t bts < <( aplay -L )
+lines=$(aplay -L | wc -l )
+let lines='lines-1'
+if [ $lines -ne 0 ]; then
+    for i in $( eval echo {0..$lines});do
+        if echo "${bts[$i]}" | grep -q a2dp ; then
+            name=$(echo ${bts[$i+1]} | cut -d "," -f 1)
+            device=$(echo ${bts[$i]})
+            echo "audio_output {" >> $file
+            echo -ne "        name            \"" >> $file
+            echo -ne $name >> $file
+            echo "\"" >> $file
+            echo "        type            \"alsa\"" >> $file
+            echo -ne "        device          \"" >> $file
+            echo -ne $device >> $file
+            echo "\"" >> $file
+            echo "        mixer_type      \"software\"" >> $file
+            echo "        auto_resample   \"no\"" >> $file
+            echo "        auto_format     \"no\"" >> $file
+            echo "        enabled         \"yes\"" >> $file
+            echo "}" >> $file
         fi
- systemctl start mpd.socket
+    done
+fi
+systemctl start mpd.socket
