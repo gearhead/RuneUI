0c8534dd4be9cd0fdd6cd48acf944eb728cf8051
janui <janui@onmat.com>
2024-07-29 11:15:55 +0200

    Fix html link to the RuneAudio forum

--- app/templates/debug.php
+++ app/templates/debug.php
@@ -2,7 +2,7 @@
     <h1>DEBUG DATA</h1>
     <div class="boxed">
         <p>The raw output of RuneUI's debug section is displayed below. It contains important information that could help to diagnose problems.<br>
-        Please copy and paste the relevant parts it into your posts if you see something which may help. See the <a href="www.runeaudio.com/forum/" title="RuneAudio Forum" target="forum">forum</a>.</p>
+        Please copy and paste the relevant parts it into your posts if you see something which may help. See the <a href="https://www.runeaudio.com/forum/raspberry-pi-f7.html" title="RuneAudio Forum" target="forum">forum</a>.</p>
         <form id="debug-buttons" class="button-list" method="post">
             <button class="btn btn-primary btn-lg" id="refresh" name="refresh" value="1" type="submit"> Refresh</button> &nbsp;
             <button class="btn btn-primary btn-lg"id="copyText" name="copyText" type="button"><i class="fa fa-copy sx"></i> Copy data to clipboard</button>
