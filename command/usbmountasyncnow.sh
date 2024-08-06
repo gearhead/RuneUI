84f0d745879ecf27591892a3221cf8b7818e08ad
janui <janui@onmat.com>
2024-08-06 16:06:53 +0200

    USB mount/ dismount improvements

---
+++ command/usbmountasyncnow.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+#  Copyright (C) 2013-2014 RuneAudio Team
+#  http://www.runeaudio.com
+#
+#  RuneUI
+#  copyright (C) 2013-2014 – Andrea Coiutti (aka ACX) & Simone De Gregori (aka Orion)
+#
+#  RuneOS
+#  copyright (C) 2013-2014 – Simone De Gregori (aka Orion) & Carmelo San Giovanni (aka Um3ggh1U)
+#
+#  RuneAudio website and logo
+#  copyright (C) 2013-2014 – ACX webdesign (Andrea Coiutti)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with RuneAudio; see the file COPYING. If not, see
+#  <http://www.gnu.org/licenses/gpl-3.0.txt>.
+#
+#  file: command/usbmountasyncnow.sh
+#  version: 0.6
+#  coder: janui
+#  date: August 2024
+#
+# udevil cannot run commands with a parameter, this script simulates running the script usbmount with the async parameter
+
+/srv/http/command/usbmount asyncnow
+#---
+#End script
