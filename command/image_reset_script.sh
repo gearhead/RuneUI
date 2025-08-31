7126e277801be6d474b095ddfbf6190dce8db84c
janui <janui@onmat.com>
2025-08-31 11:55:59 +0200

    Move dnsmasq config file and disable dnsmasq by default
    The runeaudio managed dnsmasq file has been moved from '/etc/dnsmasq.conf' to '/etc/dnsmasq.d/runeap.conf'

--- command/image_reset_script.sh
+++ command/image_reset_script.sh
@@ -107,7 +107,7 @@ rm -rf /var/lib/bluetooth/*
 # set up services and stop them
 # systemctl sometimes stops after an erroneous entry, use arrays to run through all entries individually
 declare -a disable_arr=(ashuffle bluealsa bluealsa-aplay bluealsa-monitor bluetooth bluetooth-agent bluetoothctl_scan\
-    bootsplash bt_mon_switch bt_scan_output chronyd connman-wait-online cron cronie dhcpcd dphys-swapfile\
+    bootsplash bt_mon_switch bt_scan_output chronyd connman-wait-online cron cronie dhcpcd dnsmasq dphys-swapfile\
     haveged hciuart hostapd llmnrd local-browser local-browser-w ModemManager mosquitto mpd mpdscribble nmb nmbd ntpd owntone owntone_monitor\
     owntone@.service pcscd php7.4-fpm php8.2-fpm php8.4-fpm php-fpm plymouth-lite-halt plymouth-lite-poweroff plymouth-lite-reboot\
     plymouth-lite-start redis-server rpi-display-backlight rsyslog rune_PL_wrk rune_shutdown rune_SSM_wrk samba-ad-dc\
@@ -116,7 +116,7 @@ declare -a disable_arr=(ashuffle bluealsa bluealsa-aplay bluealsa-monitor blueto
 declare -a enable_arr=(amixer-webui avahi-daemon cmd_async_queue connman dbus fix_ethx fix_usbdevices fix_wlanx iwd mpdversion nginx\
     redis rune_SY_wrk sshd systemd-journald systemd-resolved systemd-timesyncd udevil)
 declare -a stop_arr=(amixer-webui ashuffle bluealsa bluealsa-aplay bluealsa-monitor bluetooth bluetooth-agent\
-    bluetoothctl_scan bootsplash bt_mon_switch bt_scan_output chronyd cmd_async_queue connman-wait-online cron cronie dhcpcd\
+    bluetoothctl_scan bootsplash bt_mon_switch bt_scan_output chronyd cmd_async_queue connman-wait-online cron cronie dhcpcd dnsmasq\
     dphys-swapfile fix_ethx fix_usbdevices haveged hciuart llmnrd local-browser local-browser-w ModemManager mosquitto mpd mpdversion nmb\
     nmbd owntone owntone_monitor owntone@.service pcscd php7.4-fpm php8.2-fpm php8.4-fpm php-fpm plymouth-lite-halt\
     plymouth-lite-poweroff plymouth-lite-reboot\
@@ -887,6 +887,17 @@ else
     cp /usr/local/bin/ply-image /usr/bin/ply-image
     rm /usr/local/bin/ply-image
     chmod 755 /usr/bin/ply-image
+fi
+#
+# dnsmasq config file
+# previously we used and edited the file /etc/dnsmasq.conf we now use /etc/dnsmasq.d/runeap.conf
+# if the file /etc/dnsmasq.conf exists and it is the same as /etc/dnsmasq.d/runeap.conf delete /etc/dnsmasq.conf
+if [ -f "/etc/dnsmasq.conf" ] && [ -f "/etc/dnsmasq.d/runeap.conf" ] ; then
+    md5dnsmasq=$( md5sum /etc/dnsmasq.conf | xargs | cut -f 1 -d " " )
+    md5runeap=$( /etc/dnsmasq.d/runeap.conf | xargs | cut -f 1 -d " " )
+    if [ "$md5dnsmasq" == "$md5runeap" ] ; then
+        rm /etc/dnsmasq.conf
+    fi
 fi
 #
 # it is possible that the following line is required to correct a bug in the chromium singelton set up
